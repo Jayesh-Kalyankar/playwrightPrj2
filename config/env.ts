833facc268c0c8e702c48dd2c0d58a534f7349b4
Jayesh Sanjay Kalyankar <jkalyankar@academian.com>
2026-01-21 16:30:10 +0530

    Initial commit

---
+++ config/env.ts
@@ -0,0 +1,4 @@
+// config/env.ts
+export const ENV = {
+baseURL: process.env.BASE_URL || 'https://example.com',
+};
---
+++ config/playwright.config.ts
@@ -0,0 +1,14 @@
+// playwright.config.ts
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  timeout: 30 * 1000,
+  use: {
+    baseURL: process.env.BASE_URL,
+    headless: true,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+  },
+  reporter: [['html', { outputFolder: 'reports' }]],
+});
